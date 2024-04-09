996b4e2f794dc3a74f6dfc6229fa80c6c1a279b9
sadiahai <145081111+sadiahai@users.noreply.github.com>
2024-04-09 08:34:49 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 // Hamburger Menu for mobile (used W3 schools)
 function myFunction() {
     var x = document.getElementById("Links");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
+    if (x.style.display === "none" || x.style.display === "") {
         x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
 }
 
 // Hiding elements
+function hideParagraphs() {
+    var paragraphs = document.getElementsByTagName("p");
+    var button = document.querySelector("button");
 
+    for (var i = 0; i < paragraphs.length; i++) {
+        if (paragraphs[i].style.display === "none" || paragraphs[i].style.display === "") {
+            paragraphs[i].style.display = "block";
+            button.textContent = "Hide";
+        } else {
+            paragraphs[i].style.display = "none";
+            button.textContent = "Unhide";
+        }
+    }
+}
