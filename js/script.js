a4b4ccf3defd730b1ed8adf86dcce60cb60eba05
sadiahai <145081111+sadiahai@users.noreply.github.com>
2024-04-09 07:49:39 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Hamburger Menu for mobile (used W3 schools)
+function myFunction() {
+    var x = document.getElementById("Links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+// Hiding elements
+
